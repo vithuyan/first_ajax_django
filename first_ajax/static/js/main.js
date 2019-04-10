595df95e8b1f28d846f364e98d3ff1230304124d
Vithuyan Sivananthan <vithuyan.s@outlook.com>
2019-04-10 15:33:20 -0400

    work in progress

--- first_ajax/static/js/main.js
+++ first_ajax/static/js/main.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
-  
+  const getDataBtn = document.getElementById("data_button");
+     console.log('Get more data', getDataBtn);
+
+
+ getDataBtn.addEventListener('click', function() {
+    console.log("Button Click ")
+      const request = axios.get('http://intro-ajax-api.herokuapp.com/')
 });
